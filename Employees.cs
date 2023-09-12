d3d0e80c70e18cad76a87b4c1444b404a5cff104
Lailatul Qomariah <55173357+Lailatul-qomariah@users.noreply.github.com>
2023-09-12 17:20:13 +0700

    TugasGanjilGenap
    coding tugas ganjil genap hanya pada file Program.cs

---
+++ Employees.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TugasMcc
+{
+    public class Employees : Person
+    {
+        string noHp;
+        public string Umur { get; set; }
+        public Employees(string name, string address, string noHp, string umur) : base(name, address)  //untuk di new objectnya
+        {
+            this.noHp = noHp;
+            Umur = umur;
+        }
+
+        public override void Disply()
+        {
+            base.Disply();
+            Console.WriteLine("No Hp: {0}", noHp); //{0}, $ penggantinya +
+            Console.WriteLine($"umur  : {Umur}");
+
+        }
+    
+    
+    
+    
+    }
+
+
+
+}
---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasMcc
+{
+    public class Person
+    {
+        string name;
+        string address;
+        
+        public Person() { 
+        
+        }
+
+        public Person(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+
+        public virtual void Disply() // ini override sama method Disply di class Employees
+        {
+            Console.WriteLine("halo ini saya");
+            Console.WriteLine("Nama saya : {0}", name);
+            Console.WriteLine("alamat  : " + address);
+        }
+
+    }
+}
